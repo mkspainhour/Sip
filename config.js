71e74a7969b736e5b2d4f449001733d85fe6fd78
Marshall Spainhour <mkspainhour@users.noreply.github.com>
2019-02-08 17:56:52 -0600

    ONCE MORE, WITH FEELING

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "mongodb://localhost/test-sip",
-   DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/dev-sip"
+   DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost/dev-sip",
    PORT: process.env.PORT || 4444,
    JWT_SECRET: process.env.JWT_SECRET || "sip-dev-jwt-secret",
    COOKIE_EXPIRY: process.env.COOKIE_EXPIRY || new Date( Date.now()+(2)*24*60*60*1000 ),
